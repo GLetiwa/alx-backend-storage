a662c988a3150c1a3c98e6598a85a4a39ec85af8
GLetiwa <graceletiwa@gmail.com>
2024-04-25 19:51:23 +0300

    function creation

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,13 @@
+-- Creates a function that divides the first by the second num
+DELIMITER //
+
+DROP FUNCTION IF EXISTS SafeDiv;
+CREATE FUNCTION SafeDiv (
+a INT,
+b INT)
+RETURNS FLOAT DETERMINISTIC
+BEGIN
+	RETURN (IF (b = 0, 0, a / b));
+END //
+
+DELIMITER;
