d10f6fe2cddc83dcfe458f280617c882d2efbb46
GLetiwa <graceletiwa@gmail.com>
2024-04-25 14:18:32 +0300

    Old school band

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,29 @@
+-- Create AddBonus stored procedure
+DELIMITER //
+
+CREATE PROCEDURE AddBonus(
+    IN p_user_id INT,
+    IN p_project_name VARCHAR(255),
+    IN p_score INT
+)
+BEGIN
+    DECLARE p_project_id INT;
+
+    -- Check if project exists
+    SELECT id INTO p_project_id
+    FROM projects
+    WHERE name = p_project_name;
+
+    -- If project does not exist, create it
+    IF p_project_id IS NULL THEN
+        INSERT INTO projects (name)
+        VALUES (p_project_name);
+        SET p_project_id = LAST_INSERT_ID();
+    END IF;
+
+    -- Add correction
+    INSERT INTO corrections (user_id, project_id, score)
+    VALUES (p_user_id, p_project_id, p_score);
+END //
+
+DELIMITER ;
